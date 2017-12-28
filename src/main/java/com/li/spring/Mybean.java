b021196e113b46ba0058b5a3f444b060c12102ca
fjl121029xx <sishengqikuo_xx@163.com>
2017-12-28 11:08:23 +0800

    spring最初配置文件

--- src/main/java/com/li/spring/Mybean.java
+++ src/main/java/com/li/spring/Mybean.java
@@ -1,5 +1,6 @@
 package com.li.spring;
 
+import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Mybean {
@@ -10,18 +11,26 @@ public class Mybean {
         System.out.println(context.getBean(Mybean.class));
         //System.out.println(context.getBean("createMyBean"));
         System.out.println(context.getBean("defined-name"));
-        System.out.println("-----------------------------------");
 
-        /*System.out.println(context.getBean(CayenneFactoryBean.class));
+        context.close();
+    }
+
+    @Test
+    public void beanFactory() throws Exception {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+
+        System.out.println(context.getBean(CayenneFactoryBean.class));
         System.out.println(context.getBean("&getCayenneFactoryBean"));
 
         System.out.println(context.getBean("getCayenneFactoryBean"));
         System.out.println(context.getBean(Cayenne.class));
-        System.out.println("-----------------------------------");*/
-
 
 
         context.close();
     }
+
+
 }
 
